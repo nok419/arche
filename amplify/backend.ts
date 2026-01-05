4590aa61cc0e1183ea4cc4f34f32b747588c5c77
nok419 <naokifujimoto419@gmail.com>
2026-01-05 19:04:14 +0900

    0.9

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,12 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 
+/**
+ * Arche Backend Configuration
+ * - auth: Cognito User Pool (email login, groups: admin/teacher/researcher)
+ * - data: AppSync GraphQL API with DynamoDB
+ * - storage: S3 bucket for assets
+ */
 defineBackend({
   auth,
   data,
