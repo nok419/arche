52ac581e3a7abaf83985b92295aa8231e270bc52
nok419 <naokifujimoto419@gmail.com>
2026-01-05 18:38:11 +0900

    0.8

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
-// import { auth } from './auth/resource'; // 一時的に無効化
+import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 
 defineBackend({
-  // auth, // 一時的に無効化 - User Pool 削除のため
+  auth,
   data,
   storage,
 });
